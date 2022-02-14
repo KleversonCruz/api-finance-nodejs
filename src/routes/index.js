8dc842a894aa167a88f2970aa842ce3d8a7ba454
kleverson.cruz <kleverson.marinho@outlook.com>
2022-02-13 23:46:04 -0300

    Adicionado redirect para documentação postman

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,10 @@ const resumo = require('./resumoRoutes');
 const usuarios = require('./usuariosRoutes');
 
 module.exports = (app) => {
+  app.route('/').get(function (req, res) {
+    res.redirect('https://documenter.getpostman.com/view/16890150/UVeNm2w8');
+  });
+
   despesas(app);
   receitas(app);
   resumo(app);
